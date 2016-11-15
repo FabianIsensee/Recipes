cce5387903f1d5b49d0b7c2289bd8c7b66f5aee7
Fabian Isensee <f.isensee@dkfz-heidelberg.de>
2016-11-15 17:45:31 +0100

    added unpool und deconv unets

--- modelzoo/Unet.py
+++ modelzoo/Unet.py
@@ -1,6 +1,6 @@
 __author__ = 'Fabian Isensee'
 from collections import OrderedDict
-from lasagne.layers import InputLayer, ConcatLayer, Pool2DLayer, ReshapeLayer, DimshuffleLayer, NonlinearityLayer, DropoutLayer, Upscale2DLayer, batch_norm
+from lasagne.layers import InputLayer, ConcatLayer, Pool2DLayer, ReshapeLayer, DimshuffleLayer, NonlinearityLayer, DropoutLayer, Upscale2DLayer, batch_norm, Deconv2DLayer, InverseLayer
 from lasagne.layers.dnn import Conv2DDNNLayer as ConvLayer
 import lasagne
 
@@ -159,6 +159,144 @@ def build_UNet_relu_BN(n_input_channels=1, BATCH_SIZE=None, num_output_classes=2
     net['upscale4'] = Upscale2DLayer(net['expand_3_2'], 2)
 
     net['concat4'] = ConcatLayer([net['upscale4'], net['contr_1_2']], cropping=(None, None, "center", "center"))
+    net['expand_4_1'] = batch_norm(ConvLayer(net['concat4'], base_n_filters, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['expand_4_2'] = batch_norm(ConvLayer(net['expand_4_1'], base_n_filters, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+
+    net['output_segmentation'] = ConvLayer(net['expand_4_2'], num_output_classes, 1, nonlinearity=None)
+    net['dimshuffle'] = DimshuffleLayer(net['output_segmentation'], (1, 0, 2, 3))
+    net['reshapeSeg'] = ReshapeLayer(net['dimshuffle'], (num_output_classes, -1))
+    net['dimshuffle2'] = DimshuffleLayer(net['reshapeSeg'], (1, 0))
+    net['output_flattened'] = NonlinearityLayer(net['dimshuffle2'], nonlinearity=lasagne.nonlinearities.softmax)
+
+    return net
+
+def build_UNet_relu_BN_deconv(n_input_channels=1, BATCH_SIZE=None, num_output_classes=2, pad='same', input_dim=(128, 128), base_n_filters=64, do_dropout=False):
+    nonlinearity = lasagne.nonlinearities.rectify
+    net = OrderedDict()
+    net['input'] = InputLayer((BATCH_SIZE, n_input_channels, input_dim[0], input_dim[1]))
+
+    net['contr_1_1'] = batch_norm(ConvLayer(net['input'], base_n_filters, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['contr_1_2'] = batch_norm(ConvLayer(net['contr_1_1'], base_n_filters, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['pool1'] = Pool2DLayer(net['contr_1_2'], 2)
+
+    net['contr_2_1'] = batch_norm(ConvLayer(net['pool1'], base_n_filters*2, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['contr_2_2'] = batch_norm(ConvLayer(net['contr_2_1'], base_n_filters*2, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['pool2'] = Pool2DLayer(net['contr_2_2'], 2)
+
+    net['contr_3_1'] = batch_norm(ConvLayer(net['pool2'], base_n_filters*4, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['contr_3_2'] = batch_norm(ConvLayer(net['contr_3_1'], base_n_filters*4, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['pool3'] = Pool2DLayer(net['contr_3_2'], 2)
+
+    net['contr_4_1'] = batch_norm(ConvLayer(net['pool3'], base_n_filters*8, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['contr_4_2'] = batch_norm(ConvLayer(net['contr_4_1'], base_n_filters*8, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    l = net['pool4'] = Pool2DLayer(net['contr_4_2'], 2)
+    # the paper does not really describe where and how dropout is added. Feel free to try more options
+    if do_dropout:
+        l = DropoutLayer(l, p=0.4)
+
+    net['encode_1'] = batch_norm(ConvLayer(l, base_n_filters*16, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['encode_2'] = batch_norm(ConvLayer(net['encode_1'], base_n_filters*16, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['upscale1'] = batch_norm(Deconv2DLayer(net['encode_2'], base_n_filters*16, 2, 2, W=lasagne.init.HeNormal(gain="relu"), crop='full'))
+
+    net['concat1'] = ConcatLayer([net['upscale1'], net['contr_4_2']], cropping=(None, None, "center", "center"))
+    net['expand_1_1'] = batch_norm(ConvLayer(net['concat1'], base_n_filters*8, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['expand_1_2'] = batch_norm(ConvLayer(net['expand_1_1'], base_n_filters*8, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['upscale2'] = batch_norm(Deconv2DLayer(net['expand_1_2'], base_n_filters*16, 2, 2, W=lasagne.init.HeNormal(gain="relu"), crop='full'))
+
+    net['concat2'] = ConcatLayer([net['upscale2'], net['contr_3_2']], cropping=(None, None, "center", "center"))
+    net['expand_2_1'] = batch_norm(ConvLayer(net['concat2'], base_n_filters*4, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['expand_2_2'] = batch_norm(ConvLayer(net['expand_2_1'], base_n_filters*4, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['upscale3'] = batch_norm(Deconv2DLayer(net['expand_2_2'], base_n_filters*16, 2, 2, W=lasagne.init.HeNormal(gain="relu"), crop='full'))
+
+    net['concat3'] = ConcatLayer([net['upscale3'], net['contr_2_2']], cropping=(None, None, "center", "center"))
+    net['expand_3_1'] = batch_norm(ConvLayer(net['concat3'], base_n_filters*2, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['expand_3_2'] = batch_norm(ConvLayer(net['expand_3_1'], base_n_filters*2, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['upscale4'] = batch_norm(Deconv2DLayer(net['expand_3_2'], base_n_filters*16, 2, 2, W=lasagne.init.HeNormal(gain="relu"), crop='full'))
+
+    net['concat4'] = ConcatLayer([net['upscale4'], net['contr_1_2']], cropping=(None, None, "center", "center"))
+    net['expand_4_1'] = batch_norm(ConvLayer(net['concat4'], base_n_filters, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['expand_4_2'] = batch_norm(ConvLayer(net['expand_4_1'], base_n_filters, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+
+    net['output_segmentation'] = ConvLayer(net['expand_4_2'], num_output_classes, 1, nonlinearity=None)
+    net['dimshuffle'] = DimshuffleLayer(net['output_segmentation'], (1, 0, 2, 3))
+    net['reshapeSeg'] = ReshapeLayer(net['dimshuffle'], (num_output_classes, -1))
+    net['dimshuffle2'] = DimshuffleLayer(net['reshapeSeg'], (1, 0))
+    net['output_flattened'] = NonlinearityLayer(net['dimshuffle2'], nonlinearity=lasagne.nonlinearities.softmax)
+
+    return net
+
+from lasagne.layers.base import Layer
+import numpy as np
+class CenterCropLayer(Layer):
+    """
+    Center Crop
+    """
+    def __init__(self, incoming, out_shape, **kwargs):
+        super(CenterCropLayer, self).__init__(incoming, **kwargs)
+        self.out_shape = out_shape
+        self.input_shp = incoming.output_shape
+
+    def get_output_shape_for(self, input_shape):
+        output_shape = list(input_shape)
+        output_shape[2] = self.out_shape[0]
+        output_shape[3] = self.out_shape[1]
+        return tuple(output_shape)
+
+    def get_output_for(self, input, **kwargs):
+        lb_x = int(np.ceil((self.input_shp[2] - self.out_shape[0])/2.))
+        lb_y = int(np.ceil((self.input_shp[2] - self.out_shape[0])/2.))
+        return input[:,:,lb_x:self.out_shape[0],lb_y:self.output_shape[1]]
+
+
+
+def build_UNet_relu_BN_unpool(n_input_channels=1, BATCH_SIZE=None, num_output_classes=2, pad='same', input_dim=(128, 128), base_n_filters=64, do_dropout=False):
+    nonlinearity = lasagne.nonlinearities.rectify
+    net = OrderedDict()
+    net['input'] = InputLayer((BATCH_SIZE, n_input_channels, input_dim[0], input_dim[1]))
+
+    net['contr_1_1'] = batch_norm(ConvLayer(net['input'], base_n_filters, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['contr_1_2'] = batch_norm(ConvLayer(net['contr_1_1'], base_n_filters, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['pool1'] = Pool2DLayer(net['contr_1_2'], 2)
+
+    net['contr_2_1'] = batch_norm(ConvLayer(net['pool1'], base_n_filters*2, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['contr_2_2'] = batch_norm(ConvLayer(net['contr_2_1'], base_n_filters*2, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['pool2'] = Pool2DLayer(net['contr_2_2'], 2)
+
+    net['contr_3_1'] = batch_norm(ConvLayer(net['pool2'], base_n_filters*4, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['contr_3_2'] = batch_norm(ConvLayer(net['contr_3_1'], base_n_filters*4, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['pool3'] = Pool2DLayer(net['contr_3_2'], 2)
+
+    net['contr_4_1'] = batch_norm(ConvLayer(net['pool3'], base_n_filters*8, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['contr_4_2'] = batch_norm(ConvLayer(net['contr_4_1'], base_n_filters*8, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    l = net['pool4'] = Pool2DLayer(net['contr_4_2'], 2)
+    # the paper does not really describe where and how dropout is added. Feel free to try more options
+    if do_dropout:
+        l = DropoutLayer(l, p=0.4)
+
+    net['encode_1'] = batch_norm(ConvLayer(l, base_n_filters*16, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['encode_2'] = batch_norm(ConvLayer(net['encode_1'], base_n_filters*16, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['upscale1'] = InverseLayer(net['encode_2'], net['pool4'])
+    net['crop_1'] = CenterCropLayer(net['upscale1'], np.array(net['encode_2'].output_shape[2:])*2)
+
+    net['concat1'] = ConcatLayer([net['crop_1'], net['contr_4_2']], cropping=(None, None, "center", "center"))
+    net['expand_1_1'] = batch_norm(ConvLayer(net['concat1'], base_n_filters*8, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['expand_1_2'] = batch_norm(ConvLayer(net['expand_1_1'], base_n_filters*8, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['upscale2'] = InverseLayer(net['expand_1_2'], net['pool3'])
+    net['crop_2'] = CenterCropLayer(net['upscale2'], np.array(net['expand_1_2'].output_shape[2:])*2)
+
+    net['concat2'] = ConcatLayer([net['crop_2'], net['contr_3_2']], cropping=(None, None, "center", "center"))
+    net['expand_2_1'] = batch_norm(ConvLayer(net['concat2'], base_n_filters*4, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['expand_2_2'] = batch_norm(ConvLayer(net['expand_2_1'], base_n_filters*4, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['upscale3'] = InverseLayer(net['expand_2_2'], net['pool2'])
+    net['crop_3'] = CenterCropLayer(net['upscale3'], np.array(net['expand_2_2'].output_shape[2:])*2)
+
+    net['concat3'] = ConcatLayer([net['crop_3'], net['contr_2_2']], cropping=(None, None, "center", "center"))
+    net['expand_3_1'] = batch_norm(ConvLayer(net['concat3'], base_n_filters*2, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['expand_3_2'] = batch_norm(ConvLayer(net['expand_3_1'], base_n_filters*2, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
+    net['upscale4'] = InverseLayer(net['expand_3_2'], net['pool1'])
+    net['crop_4'] = CenterCropLayer(net['upscale4'], np.array(net['expand_3_2'].output_shape[2:])*2)
+
+    net['concat4'] = ConcatLayer([net['crop_4'], net['contr_1_2']], cropping=(None, None, "center", "center"))
     net['expand_4_1'] = batch_norm(ConvLayer(net['concat4'], base_n_filters, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
     net['expand_4_2'] = batch_norm(ConvLayer(net['expand_4_1'], base_n_filters, 3, nonlinearity=nonlinearity, pad=pad, W=lasagne.init.HeNormal(gain="relu")))
 
